12ca3bcd94f6b39527c75c880ee3ec6ac98f4a1f
Devin Jean <emailcruzjean@yahoo.com>
2023-10-24 11:51:52 -0500

    new release

--- meta.py
+++ meta.py
@@ -1,7 +1,7 @@
 import os
 
 name = 'netsblox'
-version = '0.5.15'
+version = '0.5.16'
 description = 'A python interface for accessing NetsBlox services'
 url = 'https://github.com/dragazo/NetsBlox-python'
 author = 'Devin Jean'
